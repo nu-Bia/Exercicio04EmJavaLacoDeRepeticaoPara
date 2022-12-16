78a07017a155a255eb6c5fca2f4ea590f3bdbda8
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-12-16 05:20:09 -0300

    Add files via upload

---
+++ ExercicioD.java
@@ -0,0 +1,23 @@
+//d)   Elaborar um programa que apresente no final o somatório dos valores pares existentes
+// na faixa 
+//de 1 até 500. 
+package Exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioD {
+public static void main (String[]args) {} {
+	
+int pares=0, i;
+
+for(i=1;i<=500;i++) {
+	
+if(i%2==0)
+	
+pares=pares+i;
+
+System.out.print("A soma dos 100 primeiros números é: " + pares );           
+          
+}	
+}
+}
